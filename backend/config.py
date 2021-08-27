a4ad795b8a28b1a263bbe592ff57a65c5a6f39b3
Avery <19372006+starsflower@users.noreply.github.com>
2021-08-27 09:49:20 +0100

    Add track modification config

--- backend/config.py
+++ backend/config.py
@@ -2,4 +2,5 @@ import os
 
 SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(16))
 SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///:memory:")
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET = os.getenv("JWT_SECRET", os.urandom(16))
