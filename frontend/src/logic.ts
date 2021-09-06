18278ecc1984b104eefb39814bc87c199545ad89
Avery <19372006+starsflower@users.noreply.github.com>
2021-09-06 10:18:43 +0100

    feature: Refactor editing logic and utilise buttons heavily

--- frontend/src/logic.ts
+++ frontend/src/logic.ts
@@ -5,25 +5,290 @@ import { Component } from "./components";
 import Editing, { addToolbarListener } from "./editor";
 
 
-let currentSubjectId: number;
-let currentNoteId: number;
-const editor = new Editing();
 const options = { weekday: "short", year: "numeric", month: "short", day: "numeric", hour: "2-digit", minute: "2-digit" } as const;
 
-function highlightActiveSubject() {
-    $(".subject--action.active").removeClass("active");
-    $(".subject--action[data-id=\"{0}\"]".formatUnicorn(currentSubjectId)).addClass("active");
-}
+export default class Horai {
+    currentSubjectId: number;
+    currentNoteId: number;
 
-function highlightActiveNote() {
-    $(".note--action.active").removeClass("active");
-    $(".note--action[data-id=\"{0}\"]".formatUnicorn(currentNoteId)).addClass("active");
-}
+    client: Client;
+    editor: Editing;
 
-async function refreshSubjectView(client: Client) {
-    await loadSubjects(client);
-    highlightActiveSubject();
-    await loadNotes(client, currentSubjectId);
+    constructor(client: Client) {
+        this.client = client;
+        this.editor = new Editing();
+    }
+
+    async getSubject() {
+        return await this.client.getSubject(this.currentSubjectId);
+    }
+
+    async getNote() {
+        return await (await this.getSubject()).getNote(this.currentNoteId);
+    }
+
+    // Purely display
+    highlightActiveSubject() {
+        $(".subject--action.active").removeClass("active");
+        $(".subject--action[data-id=\"{0}\"]".formatUnicorn(this.currentSubjectId)).addClass("active");
+    }
+
+    async setCurrentSubjectDisplay() {
+        const subject = await this.getSubject();
+        $("#current-subject").text(subject.name);
+        $("#current-subject-modified").text(subject.updatedOn.toLocaleString(undefined, options));
+        $("#note-column").removeClass("invisible");
+    }
+
+    clearCurrentSubjectDisplay() {
+        this.clearCurrentNoteDisplay();
+        $("#current-subject").text("");
+        $("#current-subject-modified").text("");
+        $("#note-column").addClass("invisible");
+    }
+
+    highlightActiveNote() {
+        $(".note--action.active").removeClass("active");
+        $(".note--action[data-id=\"{0}\"]".formatUnicorn(this.currentNoteId)).addClass("active");
+    }
+
+    async setCurrentNoteDisplay() {
+        const note = await this.getNote();
+        $("#current-note").text(note.title);
+        $("#current-note-modified").text(note.updatedOn.toLocaleString(undefined, options));
+        $("#editing-column").removeClass("invisible");
+    }
+
+    clearCurrentNoteDisplay() {
+        $("#current-note").text("");
+        $("#current-note-modified").text("");
+        $("#editing-column").addClass("invisible");
+    }
+
+    showSavedInfo() {
+        if (this.editor.lastSaved) {
+            bootbox.alert("Your note was last uploaded at {0}".formatUnicorn(this.editor.lastSaved.toLocaleTimeString()))
+        } else {
+            bootbox.alert("Your note has not yet been saved. It is likely newly open, or there is no note currently being edited");
+        }
+    }
+
+    showSettings() {
+        bootbox.alert("This feature is coming soon");
+    }
+
+    // Data loading
+    async loadNotesIntoDOM() {
+        // Load subject
+        if (!this.currentSubjectId) {
+            return $("#notes").empty();
+        }
+
+        const subject = await this.getSubject();
+
+        $("#notes").empty();
+        this.setCurrentSubjectDisplay();
+
+        for (const note of subject.notes) {
+            $("#notes").append(await Component("note.html", {
+                title: note.title,
+                preview: strip(note.content) || "No content",
+                id: note.id,
+                subjectId: subject.id,
+                updated: note.updatedOn.toLocaleString(undefined, options)
+            }))
+        }
+
+        this.addNoteEventHandlers();
+    }
+
+    async loadSubjectsIntoDOM() {
+        const subjects = await this.client.getSubjects();
+
+        $("#subjects").find(".subject--action").remove();
+        for (const subject of subjects) {
+            $("#subjects").append(await Component("subject.html", {
+                name: subject.name,
+                note_count: subject.notes.length,
+                id: subject.id,
+                updated: subject.updatedOn.toLocaleString(undefined, options)
+            }))
+        }
+
+        this.addSubjectEventHandlers();
+    }
+
+    addNoteEventHandlers() {
+        const that = this;
+        // Set up click listener
+        $(".note--action").on("click", function () {
+            const id = $(this).data("id");
+            that.currentNoteId = id;
+            that.highlightActiveNote();
+
+            that.loadNote();
+            goToNote();
+        });
+
+        $(".note--action .delete-this").on("click", function (event) {
+            event.stopPropagation();
+            that.deleteNoteCallback(event, $(this).data("id"));
+        });
+
+        $(".note--action .edit-this").on("click", function (event) {
+            event.stopPropagation();
+            that.editNoteCallback(event, $(this).data("id"));
+        });
+    }
+
+    addSubjectEventHandlers() {
+        const that = this;
+        $(".subject--action").on("click", function () {
+            // Remove any active labels...
+            const id = $(this).data("id");
+            if (id !== that.currentSubjectId) {
+                that.editor.save().then(() => {
+                    that.editor.destroyEditor();
+                    that.clearCurrentNoteDisplay();
+                });
+            }
+
+            that.currentSubjectId = id;
+            that.highlightActiveSubject();
+
+            that.loadNotesIntoDOM();
+            goToNotes();
+        })
+
+        $(".subject--action .delete-this").on("click", function (event) {
+            event.stopPropagation();
+            that.deleteSubjectCallback(event, $(this).data("id"));
+        });
+
+        $(".subject--action .edit-this").on("click", function (event) {
+            event.stopPropagation();
+            that.editSubjectCallback(event, $(this).data("id"));
+        });
+    }
+
+
+    addEventHandlers() {
+        $("#go-to-subjects").on("click", (event) => goToSubjects());
+        $("#go-to-notes").on("click", (event) => goToNotes());
+
+        $("#add-subject").on("click", (event) => this.addSubjectCallback(event));
+        $("#add-note").on("click", (event) => this.addNoteCallback(event));
+
+        $("#edit-subject").on("click", (event) => this.editSubjectCallback(event));
+        $("#edit-note").on("click", (event) => this.editNoteCallback(event));
+
+        $("#delete-subject").on("click", (event) => this.deleteSubjectCallback(event));
+        $("#delete-note").on("click", (event) => this.deleteNoteCallback(event));
+
+        $("#saved").on("click", (event) => this.showSavedInfo());
+        $("#settings-btn").on("click", (event) => this.showSettings());
+        $("#logout").on("click", (event) => this.logout());
+    }
+
+    async refreshSubjectView() {
+        await this.loadSubjectsIntoDOM();
+        this.highlightActiveSubject();
+        await this.loadNotesIntoDOM();
+    }
+
+    async loadNote() {
+        addToolbarListener(this.editor);
+        this.editor.destroyEditor();
+
+        const note = await (await this.client.getSubject(this.currentSubjectId)).getNote(this.currentNoteId);
+        this.setCurrentNoteDisplay();
+        this.editor.createEditor(note);
+    }
+
+    // Auth flow
+    async logout() {
+        await this.client.logout();
+        location.reload();
+    }
+
+    // Data control
+    addSubjectCallback(event: JQuery.ClickEvent) {
+        bootbox.prompt("Subject name?", async (result: string) => {
+            if (result !== null) {
+                await this.client.createSubject({ name: result });
+                await this.loadSubjectsIntoDOM();
+            }
+        })
+    }
+
+    addNoteCallback(event: JQuery.ClickEvent) {
+        if (this.currentSubjectId) {
+            bootbox.prompt("Note title?", async (result: string) => {
+                if (result !== null) {
+                    await (await this.client.getSubject(this.currentSubjectId)).createNote({ title: result, content: "", label: [] });
+                    await this.refreshSubjectView();
+                }
+            })
+        }
+    }
+
+    editSubjectCallback(event: JQuery.ClickEvent, subjectId?: number) {
+        const targetSubjectId = subjectId || this.currentSubjectId;
+        if (targetSubjectId) {
+            bootbox.prompt("New subject name?", async (result: string) => {
+                if (result !== null) {
+                    await (await this.client.getSubject(targetSubjectId)).setName(result);
+                    await this.loadSubjectsIntoDOM();
+                }
+            });
+        }
+    }
+
+    editNoteCallback(event: JQuery.ClickEvent, noteId?: number) {
+        const targetNoteId = noteId || this.currentNoteId;
+        if (targetNoteId) {
+            bootbox.prompt("New note title?", async (result: string) => {
+                if (result !== null) {
+                    const subject = await this.client.getSubject(this.currentSubjectId);
+                    await (await subject.getNote(targetNoteId)).setTitle(result);
+                    await this.refreshSubjectView();
+                }
+            })
+        }
+    }
+
+    deleteSubjectCallback(event: JQuery.ClickEvent, subjectId?: number) {
+        const targetSubjectId = subjectId || this.currentSubjectId;
+        if (targetSubjectId) {
+            bootbox.confirm("Are you sure you want to delete this subject and all of its notes?", async (result: boolean) => {
+                if (result) {
+                    await this.client.api.subject.delete(targetSubjectId);
+                    this.currentSubjectId = null;
+                    await this.refreshSubjectView();
+                    this.clearCurrentSubjectDisplay();
+                }
+
+            })
+        }
+    }
+
+    deleteNoteCallback(event: JQuery.ClickEvent, noteId?: number) {
+        const targetNoteId = noteId || this.currentNoteId;
+        if (targetNoteId) {
+            bootbox.confirm("Are you sure you want to delete this note?", async (result: boolean) => {
+                if (result) {
+                    this.editor.destroyEditor();
+                    await this.client.api.note.delete(this.currentSubjectId, targetNoteId);
+                    this.currentNoteId = null;
+                    await this.refreshSubjectView();
+                    this.clearCurrentNoteDisplay();
+                }
+
+            })
+        }
+    }
+
+
 }
 
 function goToSubjects() {
@@ -44,235 +309,7 @@ function goToNote() {
     $("#editing-column").addClass("d-block").removeClass("d-none");
 }
 
-function headerSetCurrentSubject(subject: Subject) {
-    $("#current-subject").text(subject.name);
-    $("#current-subject-modified").text(subject.updatedOn.toLocaleString(undefined, options));
-}
-
-function headerSetCurrentNote(note: Note) {
-    $("#current-note").text(note.title);
-}
-
-function headerClearCurrentNote() {
-    $("#current-note").text("");
-}
-
-function headerClearCurrentSubject() {
-    headerClearCurrentNote();
-    $("#current-subject").text("");
-}
-
-function showSavedInfo() {
-    if (editor.lastSaved) {
-        bootbox.alert("Your note was last uploaded at {0}".formatUnicorn(editor.lastSaved.toLocaleTimeString()))
-    } else {
-        bootbox.alert("Your note has not yet been saved. It is likely newly open, or there is no note currently being edited");
-    }
-}
-
-async function logout(client: Client) {
-    await client.logout();
-    location.reload();
-}
-
 function strip(html: string) {
     const doc = new DOMParser().parseFromString(html, 'text/html');
     return doc.body.textContent || "";
-}
-
-export async function beginInteractions(client: Client) {
-    // Start logic here
-    if (!client.isLoggedIn) {
-        return;
-    }
-
-    await loadSubjects(client);
-    addListeners(client);
-}
-
-async function loadSubjects(client: Client) {
-    // Load subjects and put into HTML
-    const subjects = await client.getSubjects()
-    $("#subjects").empty();
-
-    for (const subject of subjects) {
-        $("#subjects").append(await Component("subject.html", {
-            name: subject.name,
-            note_count: subject.notes.length,
-            id: subject.id,
-            updated: subject.updatedOn.toLocaleString(undefined, options)
-        }))
-    }
-
-    $(".subject--action").on("click", function () {
-        // Remove any active labels...
-        const id = $(this).data("id");
-        if (id !== currentSubjectId) {
-            editor.save().then(() => {
-                editor.destroyEditor();
-                headerClearCurrentNote();
-            });
-        }
-
-        currentSubjectId = id;
-        highlightActiveSubject();
-
-        loadNotes(client, id);
-        goToNotes();
-    })
-
-    $(".subject--action .delete-this").on("click", function (event) {
-        event.stopPropagation();
-        deleteSubjectCallback(client, event, $(this).data("id"));
-    });
-
-    $(".subject--action .edit-this").on("click", function (event) {
-        event.stopPropagation();
-        editSubjectCallback(client, event, $(this).data("id"));
-    });
-}
-
-async function loadNotes(client: Client, subjectId: number) {
-    // Load subject
-    if (!subjectId) {
-        return $("#notes").empty();
-    }
-
-    const subject = await client.getSubject(subjectId);
-    $("#notes").empty();
-    headerSetCurrentSubject(subject);
-
-    for (const note of subject.notes) {
-        $("#notes").append(await Component("note.html", {
-            title: note.title,
-            preview: strip(note.content) || "No content",
-            id: note.id,
-            subjectId: subject.id,
-            updated: note.updatedOn.toLocaleString(undefined, options)
-        }))
-    }
-
-    // Set up click listener
-    $(".note--action").on("click", function () {
-        const id = $(this).data("id");
-        currentNoteId = id;
-        highlightActiveNote();
-
-        loadNote(client, id);
-        goToNote();
-    });
-
-    $(".note--action .delete-this").on("click", function (event) {
-        event.stopPropagation();
-        deleteNoteCallback(client, event, $(this).data("id"));
-    });
-
-    $(".note--action .edit-this").on("click", function (event) {
-        event.stopPropagation();
-        editNoteCallback(client, event, $(this).data("id"));
-    });
-}
-
-
-async function loadNote(client: Client, noteId: number) {
-    addToolbarListener(editor);
-    editor.destroyEditor();
-
-    const note = await (await client.getSubject(currentSubjectId)).getNote(noteId);
-    headerSetCurrentNote(note);
-    editor.createEditor(note);
-}
-
-
-function addListeners(client: Client) {
-    $("#go-to-subjects").on("click", (event) => goToSubjects());
-    $("#go-to-notes").on("click", (event) => goToNotes());
-
-    $("#add-subject").on("click", (event) => addSubjectCallback(client, event));
-    $("#add-note").on("click", (event) => addNoteCallback(client, event));
-
-    $("#edit-subject").on("click", (event) => editSubjectCallback(client, event));
-    $("#edit-note").on("click", (event) => editNoteCallback(client, event));
-
-    $("#delete-subject").on("click", (event) => deleteSubjectCallback(client, event));
-    $("#delete-note").on("click", (event) => deleteNoteCallback(client, event));
-
-    $("#saved").on("click", (event) => showSavedInfo());
-    $("#logout").on("click", (event) => logout(client));
-}
-
-function addSubjectCallback(client: Client, event: JQuery.ClickEvent) {
-    bootbox.prompt("Subject name?", async (result: string) => {
-        if (result !== null) {
-            await client.createSubject({ name: result });
-            await loadSubjects(client);
-        }
-    })
-}
-
-function addNoteCallback(client: Client, event: JQuery.ClickEvent) {
-    if (currentSubjectId) {
-        bootbox.prompt("Note title?", async (result: string) => {
-            if (result !== null) {
-                await (await client.getSubject(currentSubjectId)).createNote({ title: result, content: "", label: [] });
-                await refreshSubjectView(client);
-            }
-        })
-    }
-}
-
-function editSubjectCallback(client: Client, event: JQuery.ClickEvent, subjectId?: number) {
-    const targetSubjectId = subjectId || currentSubjectId;
-    if (targetSubjectId) {
-        bootbox.prompt("New subject name?", async (result: string) => {
-            if (result !== null) {
-                await (await client.getSubject(targetSubjectId)).setName(result);
-                await loadSubjects(client);
-            }
-        });
-    }
-}
-
-function editNoteCallback(client: Client, event: JQuery.ClickEvent, noteId?: number) {
-    const targetNoteId = noteId || currentNoteId;
-    if (targetNoteId) {
-        bootbox.prompt("New note title?", async (result: string) => {
-            if (result !== null) {
-                const subject = await client.getSubject(currentSubjectId);
-                await (await subject.getNote(targetNoteId)).setTitle(result);
-                await refreshSubjectView(client);
-            }
-        })
-    }
-}
-
-function deleteSubjectCallback(client: Client, event: JQuery.ClickEvent, subjectId?: number) {
-    const targetSubjectId = subjectId || currentSubjectId;
-    if (targetSubjectId) {
-        bootbox.confirm("Are you sure you want to delete this subject and all of its notes?", async (result: boolean) => {
-            if (result) {
-                await client.api.subject.delete(targetSubjectId);
-                currentSubjectId = null;
-                await refreshSubjectView(client);
-            }
-
-            headerClearCurrentSubject();
-        })
-    }
-}
-
-function deleteNoteCallback(client: Client, event: JQuery.ClickEvent, noteId?: number) {
-    const targetNoteId = noteId || currentNoteId;
-    if (targetNoteId) {
-        bootbox.confirm("Are you sure you want to delete this note?", async (result: boolean) => {
-            if (result) {
-                editor.destroyEditor();
-                await client.api.note.delete(currentSubjectId, targetNoteId);
-                currentNoteId = null;
-                await refreshSubjectView(client);
-            }
-
-            headerClearCurrentNote();
-        })
-    }
 }
